aeec605adbeadb3395a62813bef1b08f6fc1fa1a
Tamizhvendan S <tamizh88@gmail.com>
2023-06-13 15:24:46 +0530

    reorganize content

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: ['./src/**/*.njk'],
+  content: ['./_includes/**/*.njk'],
 
   theme: {
     fontFamily: {
