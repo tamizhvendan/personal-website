e8176f349d671d1286c8ee09cbdb503b634613e8
Tamizhvendan S <tamizh88@gmail.com>
2023-08-11 16:12:37 +0530

    add pirsh and rename feeds url path

--- content/_data/meta.js
+++ content/_data/meta.js
@@ -1,7 +1,9 @@
+console.log("=>" + process.env.NODE_ENV);
 module.exports = {
   host: "www.tamizhvendan.in",
   siteUrl: "https://www.tamizhvendan.in",
   author: { name: "Tamizhvendan" },
   title: "Tamizhvendan's Personal Website",
+  pirschDataCode: process.env.ELEVENTY_RUN_MODE === 'build' ? "wagCvwnxqqFKt5KPEQDnKWzX5q4EFrg7" : "",
   siteDescription: "I'm Tamizhvendan. A Pragmatic, Passionate, Polyglot & Product Engineer and Software Craftsman from Chennai, India 🇮🇳."
 }
